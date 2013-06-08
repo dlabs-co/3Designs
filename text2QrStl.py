454a4a3f84302a1012f7b50e166f30589e9b8ea1
root <root@debian>
2013-06-08 19:47:18 +0200

    Deleted tests, added functionality to export to stl

--- text2QrStl.py
+++ text2QrStl.py
@@ -7,14 +7,17 @@
 import qrcode
 import sys
 import argparse
+import subprocess
 
 
 class QRGen(object):
     """
         Main class, generates qr code and scad
     """
-    def __init__(self, scale=4):
+    def __init__(self, scale=False):
         self.scale = scale
+        if not self.scale:
+            self.scale = 4
         self.data = ""
         self.qr_base = qrcode.QRCode(
             version=1,
@@ -37,28 +40,44 @@ class QRGen(object):
                         'x': 1 * column - self.qr_base.modules_count / 2,
                         'y': - 1 * row + self.qr_base.modules_count / 2
                     }
-                    result += 'cube([%(side)s, %(side)s, 1]);\n' % {
-                        'side': 1
-                    }
+                    result += 'cube([%s, %s, 1]);\n' % ("1", "1")
         result += '}\n'
         result += "scale([%s,%s,%s]){ union(){ qrcode(); " \
-                %( self.scale, self.scale, self.scale ) + \
+            % (self.scale, self.scale, self.scale) + \
             "translate([-10, -10, -1]) cube([qr_size,qr_size,1]); }}"
         return result
 
+
 def argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--filename', dest="filename", help='File to save it')
-    parser.add_argument('data', metavar='N', type=str, nargs='+', help='Qr data')
-    parser.add_argument('--scale', dest="scale", help='Scale the qr code', default=False)
+    parser.add_argument('--filename', dest="filename",
+                        help='File to save it')
+    parser.add_argument('data', metavar='N', type=str,
+                        nargs='+', help='Qr data')
+    parser.add_argument('--scale', dest="scale",
+                        help='Scale the qr code', default=False)
+    parser.add_argument('--format', dest="format",
+                        help='Format to save it on, \
+                            stl may not work on windows',
+                        choices=["stl", "scad"],
+                        default="scad")
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = argument_parser()
     qr = QRGen(args.scale)
     qr.make_qr()
+    if args.format == "stl":
+        if not args.filename:
+            print "Sorry, filename is required to export stl file"
+            sys.exit(1)
+
     if args.filename:
         with open(args.filename, "w") as file_:
             file_.write(qr.make_scad())
+        subprocess.Popen(
+            ["openscad", args.filename, "-o", args.filename + ".stl"]
+        )
     else:
-        print qr.make_scad()
+        print
